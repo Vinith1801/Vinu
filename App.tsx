a54cff13a4f8b8b2abcb7ec12e535d064d7525f6
Vinith1801 <vinithbylapudi@gmail.com>
2025-10-14 18:03:29 +0530

    Project added

---
+++ App.tsx
@@ -0,0 +1,28 @@
+// App.tsx
+
+import React from 'react';
+import { SafeAreaView, StyleSheet, Text } from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.text}>Hello World</Text>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f5fcff',
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#333',
+  },
+});
+
+export default App;
